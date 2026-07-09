19551b27267d5834f99a468e98cef911f50cabfb
Plug-17 <piu31153@gmail.com>
2026-07-09 14:56:24 +0700

    first edit

--- Lab02/app.js
+++ Lab02/app.js
@@ -1,2 +1,17 @@
-console.log("Hello App")
+import express from "express"
+const app = express()
+const port = 3000
+
+
+app.get("/students",(req,res)=>{
+    res.send(``)
+})
+
+app.get("/",(req,res)=>{
+    console.log("not found 404")
+})
+
+app.listen(port,()=>{
+    console.log(`hello to port ${port}`)
+})
 
