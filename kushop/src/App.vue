2c5ddb479423f25cdf9c748c87e0f634000016d4
Plug-17 <piu31153@gmail.com>
2026-09-03 19:28:47 +0700

    update 3-9-2026

---
+++ kushop/src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+ <h1>KUSHOP</h1>
+ <router-link to="/product"></router-link>
+ <router-link to="/login"></router-link>
+
+ <router-view></router-view>
+</template>
+
+<style scoped></style>
---
+++ kushop/src/router/index.js
@@ -0,0 +1,22 @@
+import { createWebHashHistory,createRouter } from "vue-router";
+import TheProduct  from "@/conponents/TheProduct.vue";
+import TheLogin  from "@/conponents/TheLogin.vue";
+
+const routes = [
+    {
+        path:'/product',
+        name:'product',
+        component:TheProduct
+    },
+    {
+        path:'/login',
+        name:'Login',
+        component:TheLogin
+    }
+]
+
+const router = createRouter({
+    history:createRouter(import.meta.env.BASE_URL),routes
+})
+
+export default router
